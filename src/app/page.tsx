454159a6f41e495d64ab0ef9a454ae0cbdfbe6b1
Lucas Fusinato Zanis <lucasfusinato-17@hotmail.com>
2023-06-24 10:42:20 -0300

    refactor: home page title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import ProfileCard from '@/components/ProfileCard'
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Lucas Fusinato - Home',
+}
 
 export default function Home() {
   return (
