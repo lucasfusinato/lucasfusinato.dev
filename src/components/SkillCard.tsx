f70048e6702f999c98654a4af8a811a6de907b7c
Lucas Fusinato Zanis <lucasfusinato-17@hotmail.com>
2023-06-24 00:03:35 -0300

    style: skill card paddings

--- src/components/SkillCard.tsx
+++ src/components/SkillCard.tsx
@@ -12,8 +12,8 @@ export default function SkillCard(props: SkillCardProperties) {
             <div className="text-center w-full">
                 <h2 className="text-xl font-bold">{props.title}</h2>
             </div>
-            <div className="flex flex-row w-full gap-10">
-                <div className="flex justify-center items-center">
+            <div className="flex flex-row w-full gap-10 pb-2">
+                <div className="flex justify-center items-center pl-2">
                     <Image src={props.image} alt={props.title} title={props.title} width={80} height={80} />
                 </div>
                 <div className="flex-grow">
