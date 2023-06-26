fdf8924907a15ba61586b8193e72f18632018603
Lucas Fusinato Zanis <lucasfusinato-17@hotmail.com>
2023-06-25 23:49:43 -0300

    refactor: use next link on social media refs

--- src/components/SocialMediaLink.tsx
+++ src/components/SocialMediaLink.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 
 interface SocialMediaLinkProperties {
     link: string;
@@ -8,8 +9,8 @@ interface SocialMediaLinkProperties {
 
 export default function SocialMediaLink(props: SocialMediaLinkProperties) {
     return (
-        <a target="_blank" href={props.link} className="flex rounded-md bg-white p-1">
+        <Link target="_blank" href={props.link} className="flex rounded-md bg-white p-1">
             <Image src={props.image} alt={props.description} title={props.description} width={32} height={32} />
-        </a>
+        </Link>
     )
 }
